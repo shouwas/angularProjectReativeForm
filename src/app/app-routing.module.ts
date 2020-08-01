e67d772ea718d1d96c6c71e3fed84ceedf35bf3a
Said Houwasse <said.houwasse@gmail.com>
2020-08-01 15:46:13 -0400

    add navigation menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListEmployeesComponent } from './employee/list-employees.component';
+import { CreateEmployeeComponent } from './employee/create-employee.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path: 'list', component: ListEmployeesComponent},
+  {path: 'create', component: CreateEmployeeComponent},
+  {path: '', redirectTo: '/list', pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
