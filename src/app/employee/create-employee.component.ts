68353fa9d659fc75d9c5a953720d9b153f0c980f
Said Houwasse <said.houwasse@gmail.com>
2020-08-03 12:00:50 -0400

    Add nested form group

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -13,7 +13,12 @@ export class CreateEmployeeComponent implements OnInit {
   ngOnInit() {
     this.employeeForm = new FormGroup({
       fullName: new FormControl(),
-      email: new FormControl()
+      email: new FormControl(),
+      skills: new FormGroup({
+      skillName: new FormControl(),
+      experienceInYears: new FormControl(),
+      proficiency: new FormControl()
+      })
     });
   }
 
