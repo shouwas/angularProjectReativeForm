685ab5d36349dae7743fe63f5827a9e7d1d76d4b
Said Houwasse <said.houwasse@gmail.com>
2020-08-04 11:00:33 -0400

    using formbuilder and validators

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { FormGroup, FormControl} from '@angular/forms'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-create-employee',
@@ -7,22 +7,24 @@ import { FormGroup, FormControl} from '@angular/forms'
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
- employeeForm: FormGroup;
-  constructor() { }
+  employeeForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.employeeForm = new FormGroup({
-      fullName: new FormControl(),
-      email: new FormControl(),
-      skills: new FormGroup({
-      skillName: new FormControl(),
-      experienceInYears: new FormControl(),
-      proficiency: new FormControl()
+    this.employeeForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+      email: [''],
+      skills: this.fb.group({
+        skillName: [''],
+        experienceInYears: [''],
+        proficiency: ['beginner']
       })
     });
   }
 
+  loadDataOnClick() {
+  }
   onSubmit(): void {
-    console.log(this.employeeForm.value);
+    console.log(JSON.stringify( this.employeeForm.get('fullName').errors.minlength.actualLength));
   }
 }
